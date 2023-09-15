6f5b09e64078bbff1a69fbdb4e9a0c9776ced101
Karoline Yamamoto <karoline859@hotmail.com>
2023-09-15 19:52:28 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Pressable ,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  Button
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen(props) {
+  const { onPress, novo = 'NOVO OKIYOME' } = props;
+
+  return (
+    <SafeAreaView className="flex-1 justify-end mx-20 my-40">
+      <View className="tems-center justify-center bg-gray">
+        <View className="space-y-5">
+        <View >
+            <Button   title="NOVO OKIYOME" ></Button>
+          </View>
+            <View>
+            <Button  title="REGISTRO" ></Button>
+          </View>
+          <View>
+            <Button  title="ESTATÍSTICA" ></Button>
+          </View>
+          <View>
+            <Button  title="SOBRE" ></Button>
+          </View>
+          </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+/*     <View style={styles.container}>
+      <Text>Teste</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */
+    <NavigationContainer >
+    <Stack.Navigator  
+       screenOptions={{
+        headerMode: 'screen',
+        headerTintColor: 'white',
+        headerStyle: { backgroundColor: '#00a2ff' },
+              }}
+        >
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+
   );
 }
 
